853debe35f26fae08bfa54522b7791a07c19b802
bartek313 <73287521+bartek313@users.noreply.github.com>
2021-01-24 18:18:33 +0100

    Create mainTest.h

---
+++ mainTest.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
+#include <cjson/cJSON.h>
+
+int choice;
+
+int main(int argc, char** argv);
+void manualSteering();
+void automaticSteering();
