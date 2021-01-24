2396f992834e656bf82dab6faf13a598b1611e3a
bartek313 <73287521+bartek313@users.noreply.github.com>
2021-01-24 18:08:17 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "apiConnector.h"
+#include "jsonConverter.h"
+#include "algorithm.h"
+
+int choice;
+
+int main(int argc, char** argv);
+void manualSteering();
+void automaticSteering();
+void workModeSelection();
